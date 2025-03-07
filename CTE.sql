bb9a803298bcf9053b76dd58c65a0ce0446c5aa6
Sahar Zare <79926856+saharzare@users.noreply.github.com>
2025-03-07 17:03:30 +0330

    Update CTE.sql

--- CTE.sql
+++ CTE.sql
@@ -1,5 +1,12 @@
+USE PUBS
+  GO
 --How to delete duplicated data with CTE!
 WITH mycte AS(
 SELECT ROW_NUMBER() OVER (PARTITION BY TITLE, TYPE, PRICE ORDER BY [title_id]) AS ROWNUM FROM titles
 )
 DELETE mycte WHERE ROWNUM >1;
+---------------------------------------------------------------------------
+--Price Deviation Analysis by Book Type Using Window Functions
+WITH CTE2 AS(
+  SELECT TITLE,TYPE,PRICE, AVG(PRICE) OVER(PARTITION BY TYPE ORDER BY PRICE DESC) AS AVG_PRICE FROM TITLES)
+SELECT CTE2.AVG_PRICE-CTE2.PRICE AS DIFF,CTE2.title,CTE2.TYPE FROM CTE2;
